851f44fa8e788939a97ab4c7d853bbccdcdf4539
Damian Rouson <rouson@lbl.gov>
2026-02-24 16:49:46 -0800

    doc: add claude-generated, qwen-prompted code

---
+++ example/vibe-coding/01-most-detailed/claude_prompted_by_qwen.f90
@@ -0,0 +1,46 @@
+module claude_prompted_by_qwen_integrand_operands_m
+  implicit none
+contains
+
+  pure function scalar(x) result(f)
+    double precision, intent(in) :: x(:)
+    double precision, allocatable :: f(:)
+    f = (x**2)/2
+  end function
+
+  pure function vector(x) result(v)
+    double precision, intent(in) :: x(:)
+    double precision, allocatable :: v(:)
+    v = x
+  end function
+
+end module
+
+program claude_prompted_by_qwen_target
+  use formal_m, only : scalar_1D_t, vector_1D_t, scalar_1D_initializer_i, vector_1D_initializer_i
+  use claude_prompted_by_qwen_integrand_operands_m, only : scalar, vector
+  implicit none
+
+  procedure(scalar_1D_initializer_i), pointer :: scalar_1D_initializer => scalar
+  procedure(vector_1D_initializer_i), pointer :: vector_1D_initializer => vector
+
+  integrand_factors: &
+  associate( &
+     f => scalar_1D_t(scalar_1D_initializer, order=4, cells=200, x_min=0D0, x_max=1D0) &
+    ,v => vector_1D_t(vector_1D_initializer, order=4, cells=200, x_min=0D0, x_max=1D0) &
+  )
+    differential_elements: &
+    associate( &
+       dV => f%dV() &
+      ,dA => v%dA() &
+    )
+      volume_integral: &
+      associate( &
+         !volume_term => dV .SSS. (v .dot. (.grad. f)) & ! <-- claude-generated
+          volume_term => .SSS. (v .dot. (.grad. f)) * dV & ! <-- manually corrected
+      )
+      end associate volume_integral
+    end associate differential_elements
+  end associate integrand_factors
+
+end program claude_prompted_by_qwen_target
