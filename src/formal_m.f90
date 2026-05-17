662eec327668254910f443b4e908b755384cd540
Damian Rouson <rouson@lbl.gov>
2026-05-17 01:10:03 -0700

    chore: reintroduce only clause in formal_m

--- src/formal_m.f90
+++ src/formal_m.f90
@@ -18,10 +18,12 @@ module formal_m
     ,d_dx   & ! scalar_1D_t spatial derivative
     ,d2_dx2   ! scalar_1D_t spatial derivative
 
-  use tensors_2D_m !, only : &
-  !   scalar_2D_t     & ! discrete 2D scalar field derived type
-  !  ,vector_2D_t     & ! discrete 2D vector field derived type
-  !  ,gradient_2D_t   & ! result of `.grad. s` for a scalar_2D_t s
+  use tensors_2D_m, only : &
+     scalar_2D_t & ! discrete 2D scalar field derived type
+    ,vector_2D_t & ! discrete 2D vector field derived type
+    ,gradient_2D_t & ! result of `.grad. s` for a scalar_2D_t s
+    ,scalar_2D_initializer_i & ! scalar_2D_t initializer abstract interface
+    ,vector_2D_initializer_i   ! vector_2D_t initializar abstract interface
 
   use differential_operators_1D_m, only : &
      gradient_operator_1D_t & ! matrix operator defining a 1D mimetic gradient
