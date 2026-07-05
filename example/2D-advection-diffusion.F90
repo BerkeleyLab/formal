9b9af6d71f4ba378738456d2eaf655bb5a8b5fd6
Damian Rouson <rouson@lbl.gov>
2026-07-05 16:39:38 -0700

    feat(2D-adv-diff): use stagnation-point velocity
    other minor renamings

--- example/2D-advection-diffusion.F90
+++ example/2D-advection-diffusion.F90
@@ -4,8 +4,6 @@
 module fields_m
   implicit none
 
-  double precision, parameter :: pi = acos(-1D0)
-
   integer, parameter :: space_dimension = 2
 
 contains
@@ -13,7 +11,8 @@ contains
   pure function scalar_field(x,y) result(gaussian)
     double precision, intent(in) :: x(:), y(:)
     double precision gaussian(size(x),size(y))
-    double precision, parameter :: x0 = pi/2, y0 = 0D0, sigma = pi/5
+    double precision, parameter :: pi = acos(-1D0)
+    double precision, parameter :: x0 = 1D0, y0 = 1D0, sigma = pi/5
     do concurrent(integer :: j=1:size(y)) default(none) shared(x,y,gaussian)
       associate(r => sqrt((x-x0)**2 + (y(j)-y0)**2))
         gaussian(:,j) = exp(-(r**2)/(2*sigma**2))
@@ -21,13 +20,14 @@ contains
     end do
   end function
 
-  pure function vector_field(x,y) result(potential_vortex)
+  pure function stagnation_point_velocity(x,y) result(grad_phi)
     double precision, intent(in) :: x(:), y(:)
-    double precision potential_vortex(size(x),size(y),space_dimension)
-    do concurrent(integer :: i=1:size(x), j=1:size(y)) default(none) shared(x,y,potential_vortex)
-      potential_vortex(i,j,:) = [-y(j)/(x(i)**2 + y(j)**2), x(i)/(x(i)**2 + y(j)**2)]
+    double precision grad_phi(size(x),size(y),space_dimension)
+    do concurrent(integer :: i=1:size(x), j=1:size(y))
+      grad_phi(i,j,:) = [x(i), -y(j)]
     end do
   end function
+
 
 end module
 
@@ -35,21 +35,21 @@ program advection_diffusion_2D
   !! Solve the advection-diffusion equation for a passive scalar moving through a static
   !! 2D velocity field.
   use julienne_m, only : file_t
-  use fields_m, only : scalar_field, vector_field, pi
+  use fields_m, only : scalar_field, stagnation_point_velocity
   use formal_m, only : scalar_2D_t, vector_2D_t, scalar_2D_initializer_i, vector_2D_initializer_i
   implicit none
 
   integer, parameter ::  order = 4
-  procedure(scalar_2D_initializer_i), pointer :: scalar_initializer
-  procedure(vector_2D_initializer_i), pointer :: velocity_initializer
+  procedure(scalar_2D_initializer_i), pointer :: scalar_2D_initializer
+  procedure(vector_2D_initializer_i), pointer :: velocity_2D_initializer
   type(scalar_2D_t) s
 
-  scalar_initializer => scalar_field
-  velocity_initializer => vector_field
+  scalar_2D_initializer => scalar_field
+  velocity_2D_initializer => stagnation_point_velocity
 
-  s = scalar_2D_t(scalar_initializer, order=order, cells=[10,20], x_min=[-pi,-pi], x_max=[pi,pi])
+  s = scalar_2D_t(scalar_2D_initializer, order=4, cells=[20,20], x_min=[-2D0,-2D0], x_max=[2D0,2D0])
 
-  associate(v => vector_2D_t(velocity_initializer, mold=s))
+  associate(v => vector_2D_t(velocity_2D_initializer, mold=s))
 
     advance_time: &
     block
